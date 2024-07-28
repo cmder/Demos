66e26064538926ef3d75a0dc942af1c557daa16b
Bruce Yao <cmd100000@gmail.com>
2024-07-28 09:17:35 +0800

    Mp4Player : delete test

--- Mp4Player/app/src/main/java/com/example/mp4player/NextActivity.java
+++ Mp4Player/app/src/main/java/com/example/mp4player/NextActivity.java
@@ -1,42 +1,60 @@
 package com.example.mp4player;
 
 import android.app.Activity;
-import android.graphics.SurfaceTexture;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NextActivity extends Activity {
-    private SurfaceView mSurfaceView;
+    private SurfaceView mSurfaceView1;
+    private SurfaceView mSurfaceView2;
     private Button mBtnRotate;
     private Button mBtnScale;
     private Button mBtnTranslate;
+    private Button mBtnPlay;
     private VideoPlayer mVideoPlayer;
+    private List<Surface> mSurfaces;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
 
-        mSurfaceView = findViewById(R.id.surfaceView);
+        mSurfaceView1 = findViewById(R.id.surfaceView1);
+        mSurfaceView2 = findViewById(R.id.surfaceView2);
         mBtnRotate = findViewById(R.id.btn_rotate);
         mBtnScale = findViewById(R.id.btn_scale);
         mBtnTranslate = findViewById(R.id.btn_translate);
+        mBtnPlay = findViewById(R.id.btn_play);
+        mSurfaces = new ArrayList<>();
 
-        mSurfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+        mSurfaceView1.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
                 Surface surface = holder.getSurface();
-                mVideoPlayer = new VideoPlayer(
-                        NextActivity.this,
-                        "android.resource://" + getPackageName() + "/" + R.raw.test,
-                        surface);
+                mSurfaces.add(surface);
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                mVideoPlayer.release();
+            }
+        });
+
+        mSurfaceView2.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                Surface surface = holder.getSurface();
+                mSurfaces.add(surface);
             }
 
             @Override
@@ -61,6 +79,18 @@ public class NextActivity extends Activity {
             mVideoPlayer.translate(0.8f, 0.8f);
         });
 
+        mBtnPlay.setOnClickListener(v -> {
+            mVideoPlayer = new VideoPlayer(
+                    NextActivity.this,
+                    "android.resource://" + getPackageName() + "/" + R.raw.test,
+                    mSurfaces);
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
 
