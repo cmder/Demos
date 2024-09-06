bc4823a05d14afee4aea52e8b777477cf12c0d18
Bruce Yao <cmd100000@gmail.com>
2024-09-06 15:51:43 +0800

    NDKdemo : Java and C++ call each other

--- NDKdemo/app/src/main/java/com/cmder/ndkdemo/MainActivity.java
+++ NDKdemo/app/src/main/java/com/cmder/ndkdemo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.cmder.ndkdemo;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    public native void callJavaFromCpp();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        callJavaFromCpp();
+    }
+
+    public String getMessageFromJava() {
+        return "Hello from Java";
+    }
+
+    public void logMessageFromCpp(String message){
+        Log.d("From C++", message);
     }
 }
