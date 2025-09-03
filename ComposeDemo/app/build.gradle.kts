9d1038ddbf4778ea0ec3316ed962bc2a3ef57e57
Bruce Yao <cmd100000@gmail.com>
2025-09-03 18:00:19 +0800

    update

--- ComposeDemo/app/build.gradle.kts
+++ ComposeDemo/app/build.gradle.kts
@@ -19,6 +19,9 @@ android {
     }
 
     buildTypes {
+        debug {
+            isDebuggable = true
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
