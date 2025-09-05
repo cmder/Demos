f039212d2a03e763cf189007edfd1e11fe861846
Bruce Yao <cmd100000@gmail.com>
2025-09-05 19:14:51 +0800

    update

--- ComposeDemo/app/src/main/java/com/cmder/composedemo/MainActivity.kt
+++ ComposeDemo/app/src/main/java/com/cmder/composedemo/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -26,6 +27,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.ColorUtils
 import com.cmder.composedemo.ui.theme.ComposeDemoTheme
@@ -49,27 +51,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Row {
         Box(
             Modifier
-                .size(50.dp)
-                .background(color = Color.Red)
-        ) {
-            Text("纯色", Modifier.align(Alignment.Center))
-        }
-        Spacer(modifier = Modifier.width(8.dp))
-        Box(
-            Modifier
-                .size(50.dp)
-                .background(
-                    brush = Brush.linearGradient( // 线性渐变
-                        colors = listOf(Color(0xFF2196F3), Color(0xFF21CBF3)) // 从蓝到浅蓝
-                    )
-                )
-        ) {
-            Text("渐变色", Modifier.align(Alignment.Center))
-        }
-    }
+                .size(100.dp)
+                .offset{
+                    IntOffset(200.dp.roundToPx(), 150.dp.roundToPx())
+                }
+                .background(Color.Red)
+        )
 }
 
 @Preview(showBackground = true)
