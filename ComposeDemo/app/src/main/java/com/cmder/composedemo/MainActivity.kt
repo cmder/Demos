b40f9a2c8d9e0adcf43320b0305873a7845cb3c1
Bruce Yao <cmd100000@gmail.com>
2025-09-05 18:00:19 +0800

    update

--- ComposeDemo/app/src/main/java/com/cmder/composedemo/MainActivity.kt
+++ ComposeDemo/app/src/main/java/com/cmder/composedemo/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.cmder.composedemo
 
+import android.icu.text.ListFormatter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.graphics.ColorUtils
 import com.cmder.composedemo.ui.theme.ComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +49,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Row {
+        Box(
+            Modifier
+                .size(50.dp)
+                .background(color = Color.Red)
+        ) {
+            Text("纯色", Modifier.align(Alignment.Center))
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Box(
+            Modifier
+                .size(50.dp)
+                .background(
+                    brush = Brush.linearGradient( // 线性渐变
+                        colors = listOf(Color(0xFF2196F3), Color(0xFF21CBF3)) // 从蓝到浅蓝
+                    )
+                )
+        ) {
+            Text("渐变色", Modifier.align(Alignment.Center))
+        }
+    }
 }
 
 @Preview(showBackground = true)
