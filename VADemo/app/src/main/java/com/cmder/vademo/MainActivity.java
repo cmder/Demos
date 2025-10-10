2f64f2600d7f6b2fc4fdd1a477e72cad91788d2a
Bruce Yao <cmd100000@gmail.com>
2025-10-10 18:00:19 +0800

    update

--- VADemo/app/src/main/java/com/cmder/vademo/MainActivity.java
+++ VADemo/app/src/main/java/com/cmder/vademo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.cmder.vademo;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,14 +10,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import io.agora.mediaplayer.IMediaPlayer;
+import io.agora.mediaplayer.IMediaPlayerObserver;
+import io.agora.mediaplayer.data.CacheStatistics;
+import io.agora.mediaplayer.data.PlayerPlaybackStats;
+import io.agora.mediaplayer.data.PlayerUpdatedInfo;
+import io.agora.mediaplayer.data.SrcInfo;
 import io.agora.rtc2.Constants;
+import io.agora.rtc2.IRtcEngineEventHandler;
 import io.agora.rtc2.RtcEngine;
 import io.agora.rtc2.RtcEngineConfig;
+import io.agora.rtc2.video.VideoCanvas;
 
-// https://doc.shengwang.cn/doc/rtc/android/advanced-features/media-player
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final String SAMPLE_MOVIE_URL = "https://agora-adc-artifacts.s3.cn-north-1.amazonaws.com.cn/resources/sample.mp4";
 
     private RtcEngine engine;
+    private IMediaPlayer mediaPlayer;
+
+    private SurfaceView surfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +43,29 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        surfaceView = findViewById(R.id.surfaceView);
+
         RtcEngineConfig config = new RtcEngineConfig();
-        config.mAppId = "8757f90c0bdb4a168a0106e4156c5dbb";
+        config.mAppId = getString(R.string.agora_app_id);
+        String channelId = "testChannel";
         config.mChannelProfile = Constants.CHANNEL_PROFILE_LIVE_BROADCASTING;
+        config.mEventHandler = new IRtcEngineEventHandler() {};
+        try {
+            engine = RtcEngine.create(config);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        mediaPlayer = engine.createMediaPlayer();
+        VideoCanvas videoCanvas = new VideoCanvas(surfaceView, Constants.RENDER_MODE_HIDDEN, 0);
+        videoCanvas.sourceType = Constants.VIDEO_SOURCE_MEDIA_PLAYER;
+        videoCanvas.mediaPlayerId = mediaPlayer.getMediaPlayerId();
+        engine.setupLocalVideo(videoCanvas);
+
+        TokenUtils.gen(this, channelId, 0, ret -> {
+            /* Allows a user to join a channel.
+             if you do not specify the uid, we will generate the uid for you*/
+            engine.joinChannel(ret, channelId, 0, options);
+        });
+
     }
 }
