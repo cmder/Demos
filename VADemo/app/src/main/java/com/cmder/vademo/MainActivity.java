4bfe20e87e78e6d75ec7077823ff75f69bdf8028
Bruce Yao <cmd100000@gmail.com>
2025-10-13 10:06:31 +0800

    update

--- VADemo/app/src/main/java/com/cmder/vademo/MainActivity.java
+++ VADemo/app/src/main/java/com/cmder/vademo/MainActivity.java
@@ -1,8 +1,8 @@
 package com.cmder.vademo;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,17 +11,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import io.agora.mediaplayer.IMediaPlayer;
-import io.agora.mediaplayer.IMediaPlayerObserver;
-import io.agora.mediaplayer.data.CacheStatistics;
-import io.agora.mediaplayer.data.PlayerPlaybackStats;
-import io.agora.mediaplayer.data.PlayerUpdatedInfo;
-import io.agora.mediaplayer.data.SrcInfo;
+import io.agora.rtc2.ChannelMediaOptions;
 import io.agora.rtc2.Constants;
 import io.agora.rtc2.IRtcEngineEventHandler;
 import io.agora.rtc2.RtcEngine;
 import io.agora.rtc2.RtcEngineConfig;
 import io.agora.rtc2.video.VideoCanvas;
 
+// 1. agora 视频播放 2. visualon 视频播放 3. 从RTMP直播流切换到VOD流播放
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -29,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RtcEngine engine;
     private IMediaPlayer mediaPlayer;
+    private ChannelMediaOptions options = new ChannelMediaOptions();
 
     private SurfaceView surfaceView;
 
@@ -47,25 +45,52 @@ public class MainActivity extends AppCompatActivity {
 
         RtcEngineConfig config = new RtcEngineConfig();
         config.mAppId = getString(R.string.agora_app_id);
-        String channelId = "testChannel";
+
         config.mChannelProfile = Constants.CHANNEL_PROFILE_LIVE_BROADCASTING;
-        config.mEventHandler = new IRtcEngineEventHandler() {};
+        config.mEventHandler = new IRtcEngineEventHandler() {
+        };
         try {
             engine = RtcEngine.create(config);
+            mediaPlayer = engine.createMediaPlayer();
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
-        mediaPlayer = engine.createMediaPlayer();
+
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void onClick(View view) {
         VideoCanvas videoCanvas = new VideoCanvas(surfaceView, Constants.RENDER_MODE_HIDDEN, 0);
         videoCanvas.sourceType = Constants.VIDEO_SOURCE_MEDIA_PLAYER;
         videoCanvas.mediaPlayerId = mediaPlayer.getMediaPlayerId();
         engine.setupLocalVideo(videoCanvas);
+        engine.setDefaultAudioRoutetoSpeakerphone(true);
 
+        engine.setClientRole(Constants.CLIENT_ROLE_BROADCASTER);
+
+        options.clientRoleType = Constants.CLIENT_ROLE_BROADCASTER;
+        options.autoSubscribeVideo = true;
+        options.autoSubscribeAudio = true;
+        options.publishScreenCaptureVideo = false;
+        options.publishCameraTrack = false;
+        options.publishMicrophoneTrack = false;
+        options.enableAudioRecordingOrPlayout = true;
+
+        String channelId = "testChannel";
         TokenUtils.gen(this, channelId, 0, ret -> {
             /* Allows a user to join a channel.
              if you do not specify the uid, we will generate the uid for you*/
             engine.joinChannel(ret, channelId, 0, options);
         });
 
+        mediaPlayer.open(SAMPLE_MOVIE_URL, 0);
+
+        mediaPlayer.play();
     }
 }
